0873d2786641d38f75880cad1b7953027b9feb36
Moses Esan <esanm@rte.ie>
2021-04-08 18:53:42 +0100

    Updated App.js

--- App.js
+++ App.js
@@ -1,21 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from 'react';
+
+import {createServer} from "miragejs"
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+import Router from './app/router';
+
+const STORAGE_KEY = "@trades";
+
+if (window.server) {
+    server.shutdown()
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+async function getTrades() {
+    let trades = await AsyncStorage.getItem(STORAGE_KEY);
+    trades = (trades !== null) ? JSON.parse(trades) : [];
+    return trades;
+}
+
+window.server = createServer({
+    routes() {
+        this.get("/api/trade", async () => {
+            try {
+                let trades = await getTrades();
+                return {trades}
+            } catch (e) {
+                return {success: false, message: e}
+            }
+        });
+
+        this.post("/api/trade", async (schema, request) => {
+            try {
+                //get the trades array
+                let trades = await getTrades();
+
+                //get the new trade data
+                let trade = JSON.parse(request.requestBody);
+
+                //push the the new trade into the trades arra
+                trades.push(trade);
+
+                //update the storage
+                await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(trades));
+
+                return {trade, message: 'Trade added successfully'};
+
+            } catch (e) {
+                return {success: false, message: e}
+            }
+        });
+
+        this.put("/api/trade/:id", async (schema, request) => {
+            try {
+                //get the id
+                let id = request.params.id;
+
+                //get the trades array
+                let trades = await getTrades();
+
+                //get the updated trade data
+                let trade = JSON.parse(request.requestBody);
+
+                //Find the trade using the id, if found update the trade using its index
+                const index = trades.findIndex((obj) => obj.id === id);
+                if (index !== -1) trades[index] = {...trades[index], ...trade};
+
+                //update the storage
+                await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(trades));
+
+                return {trade, message: 'Trade updated successfully'};
+
+            } catch (e) {
+                return {success: false, message: e}
+            }
+        });
+
+        this.del("/api/trade/:id", async (schema, request) => {
+            try {
+                //get the id
+                let id = request.params.id;
+
+                //get the trades array
+                let trades = await getTrades();
+
+                //Find the trade using the id and remove it from the trades array
+                trades = trades.filter((obj) => obj.id !== id);
+
+                //update the storage
+                await AsyncStorage.setItem(STORAGE_KEY, JSON.stringify(trades));
+
+                return {message: 'Trade deleted successfully'};
+
+            } catch (e) {
+                return {success: false, message: e}
+            }
+        })
+    },
 });
+
+export default function App() {
+    return (
+        <Router/>
+    );
+}
